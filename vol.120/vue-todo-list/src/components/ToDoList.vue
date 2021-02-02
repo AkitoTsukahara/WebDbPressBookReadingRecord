9eb1dc9c812a31cb0c4d40633a924a9e3ff96071
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-02 22:28:15 +0900

    todo app task表示

---
+++ vol.120/vue-todo-list/src/components/ToDoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="todo in todoItems" v-bind:key="todo.id">
+      {{ todo.text}}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      todoItems: [
+        { id: 1, text: 'Go out to sea.'},
+        { id: 2, text: 'Invite the first member'}
+      ]
+    }
+  }
+}
+</script>
